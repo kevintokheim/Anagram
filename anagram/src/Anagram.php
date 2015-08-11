681aee88a27e417d3800c649299ab3bdfff28525
Kevin Tokheim & Jenna Navratil <student@epicodus.com>
2015-08-11 14:22:06 -0700

    update specs, class and test files.

--- anagram/src/Anagram.php
+++ anagram/src/Anagram.php
@@ -1,26 +1,14 @@
 <?php
     class Anagram
     {
-        function makeAnagram($input_words)
+        function makeAnagram($input_word1, $input_word2)
         {
 
-            $first_word = $input_words[0];
-            $second_word = $input_words[1];
-
-            $first_string = str_split($first_word);
-            $second_string = str_split($second_word);
-
-            sort($first_array);
-            sort($second_array);
-
-            foreach ($input_words as $word) {
-
-                if ($first_string == $second_string){
-
-                }
-                array_push($output_array, $string);
+            if ($input_word1 === $input_word2) {
+                return $input_word2;
             }
-            return $input_string;
+
+
         }
     }
 
--- anagram/tests/AnagramTest.php
+++ anagram/tests/AnagramTest.php
@@ -5,30 +5,47 @@
     class AnagramTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_makeAnagram_letters()
+        function test_makeAnagram_matchedLetters()
         {
             //Arrange
             $test_Anagram = new Anagram;
-            $input = "bread";
+            $input1 = "a";
+            $input2 = "a";
 
             //Act
-            $result = $test_Anagram->makeAnagram($input);
+            $result = $test_Anagram->makeAnagram($input1, $input2);
 
             //Assert
-            $this->assertEquals(["b", "r", "e", "a", "d"], $result);
+            $this->assertEquals("a", $result);
         }
 
-        function test_makeAnagram_twoStrings()
+        function test_makeAnagram_mismatchedLetters()
         {
             //Arrange
             $test_Anagram = new Anagram;
-            $input = ['bread', 'table'];
+            $input1 = "a";
+            $input2 = "b";
 
             //Act
-            $result = $test_Anagram->makeAnagram($);
+            $result = $test_Anagram->makeAnagram($input1, $input2);
 
             //Assert
-            $this->assertEquals(true, $result);
+            $this->assertEquals("", $result);
         }
+
+        function test_makeAnagram_inverseWords()
+        {
+            //Arrange
+            $test_Anagram = new Anagram;
+            $input1 = "on";
+            $input2 = "no";
+
+            //Act
+            $result = $test_Anagram->makeAnagram($input1, $input2);
+
+            //Assert
+            $this->assertEquals("no", $result);
+        }
+
     }
 ?>
